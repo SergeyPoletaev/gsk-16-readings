3b20c066424892ccdc5ab29fe1e33012894fa0d3
spoletaev <info.elekit@yandex.ru>
2024-01-02 14:59:15 +0300

    fix аннотации валидатора

--- src/main/java/ru/gsk16/readings/model/InfoMessage.java
+++ src/main/java/ru/gsk16/readings/model/InfoMessage.java
@@ -3,11 +3,11 @@ package ru.gsk16.readings.model;
 import lombok.Data;
 import ru.gsk16.readings.enums.Template;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class InfoMessage {
     private Integer boxId;
-    @NotBlank
+    @NotNull
     private Template template;
 }
